dc30593c0b3f939f2f5e0ed40be8ba20443d5b0b
studentuser <studentuser@D-173-250-161-213.dhcp4.washington.edu>
2016-12-06 13:45:44 -0800

    twitter pic

--- ui.R
+++ ui.R
@@ -6,7 +6,8 @@ library(twitteR)
 
 fluidPage(
   verticalLayout(
-    titlePanel("Vertical layout example"),
+    img(src='twitter.png', align = "right"),
+    titlePanel("Find Trending Hashtag"),
     
     #lable output box
     plotOutput('map'),
@@ -16,22 +17,15 @@ fluidPage(
       submitButton("Submit")
     )))
 
-shinyUI(
-    fluidPage(
-       titlePanel("Insert Title here"),
-          plotOutput('map'),
-            wellPanel(
-              helpText("Look at locations of the trending hashtag"),
-              textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
-              submitButton("Submit")
-            )))
-
-
-#should we add twitter logo somewhere on the top? 
-mainPanel(
-  img(src='myImage.png', align = "right"),
-  ### the rest of your code
-)
-
-shinyUI()
-8d06760fb918ea2ed65c16bab3b7b7295820a374
+#shinyUI(
+ #   fluidPage(
+  #     titlePanel("Insert Title here"),
+   #    img(src='twitter.svg', align = "right"),
+    #      plotOutput('map'),
+     #       wellPanel(
+      #        helpText("Look at locations of the trending hashtag"),
+       #       textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
+        #      submitButton("Submit")
+         #   )))
+#shinyUI()
+#8d06760fb918ea2ed65c16bab3b7b7295820a374
