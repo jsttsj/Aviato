fded61a3f2ce12478668a89c2bdd9b79563e8c36
studentuser <studentuser@D-173-250-161-213.dhcp4.washington.edu>
2016-12-06 13:27:39 -0800

    two ui's

--- ui.R
+++ ui.R
@@ -11,8 +11,9 @@ fluidPage(
     #lable output box
     plotOutput('map'),
     wellPanel(
-      sliderInput("n", "Number of points", 10, 200,
-                  value = 50, step = 10)
+      helpText("Look at locations of the trending hashtag"),
+      textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
+      submitButton("Submit")
     )
   )
 )
@@ -26,28 +27,8 @@ shinyUI(
               textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
               submitButton("Submit")
             )))
-                   # Create a tab panel
-                   tabPanel('2',
-                            titlePanel('3'),
-                            # Create sidebar layout
-                            sidebarLayout(
-                              
-                              # Side panel for controls
-                              sidebarPanel(
-                                helpText("Look at locations of the trending hashtag"),
-                                # Create selectable input based on the type of measurement. 
-                                textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
-                                submitButton("Submit")
-                              ),
-                              
-                              # Main panel: display plotly graph
-                              mainPanel(
-                                img(src="#putintwitterimagehere", height = 20, width = 20),
-                                plotOutput('map')
-                              )
-                            )
-                   )
-))
+
+#should we add twitter logo somewhere on the top? 
 
 shinyUI()
 8d06760fb918ea2ed65c16bab3b7b7295820a374
