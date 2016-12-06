40ab60acd64b06cebef9f3da212e9e115d3816a7
studentuser <studentuser@D-173-250-161-213.dhcp4.washington.edu>
2016-12-06 13:35:01 -0800

    twitter pic added

--- ui.R
+++ ui.R
@@ -14,9 +14,7 @@ fluidPage(
       helpText("Look at locations of the trending hashtag"),
       textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
       submitButton("Submit")
-    )
-  )
-)
+    )))
 
 shinyUI(
     fluidPage(
@@ -28,7 +26,12 @@ shinyUI(
               submitButton("Submit")
             )))
 
+
 #should we add twitter logo somewhere on the top? 
+mainPanel(
+  img(src='myImage.png', align = "right"),
+  ### the rest of your code
+)
 
 shinyUI()
 8d06760fb918ea2ed65c16bab3b7b7295820a374
