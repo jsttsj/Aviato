f76faca1c70016d722037fee132005804e28937e
Jenny Liang <Jenny@ho-mba-846.local>
2016-12-07 13:29:35 -0800

    Added code to plot the US graph.

--- server.R
+++ server.R
@@ -3,9 +3,8 @@ library(dplyr)
 library(shiny)
 library(plotly)
 library(rsconnect)
-
-install.packages('twitteR')
 library(twitteR)
+
 twitteR:::setup_twitter_oauth("KgSpEvpVmBqyjaHb8NKRS2cbq",
                     "XLGHwHKXUF6rt4m3lXQNhCOVsAyzneQrznBe4INje27olbCUHO",
                     "803743164884340736-ceXBIGtqVWc8wp27jXclV82QX6gWtoT",
@@ -27,9 +26,9 @@ locations.US.nospace$name <- gsub("-", "", locations.US.nospace$name)
 city.data <- matrix(list(), nrow = 63, ncol = 1)
 dimnames(city.data) <- list(unlist(locations.US$name), c("Data"))
 
-cities <- read.csv('City_Locations.csv', stringsAsFactors = FALSE)
+cities <- read.csv("City_Locations.csv", stringsAsFactors = FALSE)
 
-for(i in 1:nrow(locations.US)){
+for(i in 1:5){
   name <- paste0('state.data.', locations.US.nospace$name[i])
   assign(name, twitteR::getTrends(locations.US.nospace$woeid[i]))
   list <- eval(parse(text = name)) %>% select(name) %>% split(seq(nrow(eval(parse(text = name)))))
@@ -37,61 +36,80 @@ for(i in 1:nrow(locations.US)){
   city.data[[i, 1]] <- list
 }
 
-# as an example of how to call something specific.  This calls the third most popular thing 
-# in Baton Rouge
-city.data[["Baton Rouge",1]]$`3`
-
 shinyServer(function(input, output) { 
   
   # Retrieve search string from user.
-  search.string <- tolower(gsub(" ", "", input$search))
-    
-  # For each of the 62 df, run through trending topics/hashtags. Stop at first instance
-  # where topic/hashtag contains input string. This will minic "related concepts" --Jenny
-  all.cities <- rownames(city.data)
-  ranking <- vector(mode="integer", length = length(all.cities))
-  related.topic <- vector(mode="character", length = length(all.cities))
-  for(city in 1:length(all.cities)) {
-    current.city <- all.cities[city]
-    most.popular <- city.data[[current.city, 1]]
-    has.match <- FALSE
-    index <- 1
-    while(has.match == FALSE && index < 21) {
-      put.related.topic <- ""
-      put.ranking <- 0
-      popular.topic <- most.popular[[index]]$name
-      if(grepl(search.string, tolower(gsub(" ", "", popular.topic))) == TRUE) {
-        put.related.topic <- popular.topic
-        put.ranking <- index
-        has.match <- TRUE
-      }
-      ranking[city] <- put.ranking
-      related.topic[city] <- put.related.topic
-      index <- index + 1
-    }
-  }
-  
-  to.plot <- cities %>%
-    select(new.name, lat, lon) %>%
-    mutate(ranking, related.topic)
+  searchString <- reactive({
+    tolower(gsub(" ", "", input$search))
+  }) 
   
   output$map <- renderPlotly({ 
+
+    to.plot <- getData()
+    g <- list(
+      scope = 'usa',
+      projection = list(type = 'albers usa'),
+      showland = TRUE,
+      landcolor = toRGB("gray85"),
+      subunitwidth = 1,
+      countrywidth = 1,
+      subunitcolor = toRGB("white"),
+      countrycolor = toRGB("white")
+    )
     
-    # Plot the city on Plotly. Display the topic/hashtag being plotted. Modify size of 
-    # plotted dot based on the row number of the topic/hashtag. --Jenny
+      p <- plot_geo(to.plot, locationmode = 'USA-states', sizes = c(1, 150), color=~ranking) %>%
+      add_markers(
+        x = ~lon, y = ~lat, hoverinfo = "text", size = ~ranking,
+        text = ~paste(to.plot$new.name, "<br />", to.plot$related.topic, "<br />", "#", 21 - to.plot$ranking)
+      ) %>%
+      layout(title = 'Trending levels of topics', geo = g) %>%
     
-    #return()
+    return()
   }) 
   
-  output$table <- renderPlotly({ 
-    
-    # Plot the city on Plotly. Display the topic/hashtag being plotted. Modify size of 
-    # plotted dot based on the row number of the topic/hashtag. --Jenny
-    
-    #return()
+  # @RICH: Make sure the UI and server elements are consistent!
+  output$table <- renderPlot({ 
+    getData() %>%
+      select("new.name", "ranking") %>%
+      return()
   }) 
+  
+  getData <- function() {
+    all.cities <- rownames(city.data)
+    ranking <- vector(mode="integer", length = length(all.cities))
+    related.topic <- vector(mode="character", length = length(all.cities))
+    for(city in 1:5) {
+      current.city <- all.cities[city]
+      most.popular <- city.data[[current.city, 1]]
+      has.match <- FALSE
+      
+      index <- 1
+      while(has.match == FALSE && index < 6) {
+        put.related.topic <- ""
+        put.ranking <- 0
+        popular.topic <- most.popular[[index]]$name
+        if(grepl(searchString(), tolower(gsub(" ", "", popular.topic))) == TRUE) {
+          put.related.topic <- popular.topic
+          put.ranking <- index
+          has.match <- TRUE
+        }
+        ranking[city] <- put.ranking
+        if(put.ranking > 0) {
+          ranking[city] <- 21 - put.ranking
+        }
+        related.topic[city] <- put.related.topic
+        index <- index + 1
+      }
+    }
+    
+    return(cities %>%
+             select(new.name, lat, lon) %>%
+             mutate(ranking, related.topic))
+  }
   
 })
+
+
 
  
   
--- ui.R
+++ ui.R
@@ -4,30 +4,30 @@ library(dplyr)
 library(rsconnect)
 library(twitteR)
 
-fluidPage(
-  verticalLayout(
-    titlePanel("Vertical layout example"),
+#fluidPage(
+  #verticalLayout(
+    #titlePanel("Vertical layout example"),
     
     #lable output box
-    plotOutput('map'),
-    wellPanel(
-      helpText("Look at locations of the trending hashtag"),
-      textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
-      submitButton("Submit")
-    )
-  )
-)
+    #plotOutput('map'),
+    #wellPanel(
+      #helpText("Look at locations of the trending hashtag"),
+      #textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
+      #submitButton("Submit")
+    #)
+  #)
+#)
 
 shinyUI(
     fluidPage(
        titlePanel("Insert Title here"),
-          plotOutput('map'),
+          plotlyOutput('map'),
             wellPanel(
-              helpText("Look at locations of the trending hashtag"),
-              textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
+              helpText("Look at locations of the trending topic"),
+              textInput("search", label = h3("Enter a topic"), value = ""),
               submitButton("Submit")
-            )))
+            )
+       )
+    )
 
 #should we add twitter logo somewhere on the top? 
-
-shinyUI()
