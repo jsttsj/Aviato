2297765f8a218542a107dae40c3df678daa1a2b2
studentuser <studentuser@studentusers-MacBook-Pro.local>
2016-12-07 11:01:27 -0800

    refurbished

--- ui.R
+++ ui.R
@@ -3,32 +3,24 @@ library(plotly)
 library(dplyr)
 library(rsconnect)
 library(twitteR)
-shinyUI(
-  fluidPage(
-    verticalLayout(
-      titlePanel("Aviato"),
-      mainPanel(
-      img(src="twitter.jpg", height = 100, width =150, align ="right")),
-   #lable output box
-    plotOutput('map'),
-    wellPanel(
-      helpText("Look at the locations of trending hashtags"),
-      textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
-      submitButton("Submit")
-    ))))
 
-# mainPanel(
- # img(src='twitter.png', height = 10, width = 10),
-  # ),
-#shinyUI(
- #   fluidPage(
-  #     titlePanel("Insert Title here"),
-   #    img(src='twitter.png', align = "right"),
-    #      plotOutput('map'),
-     #       wellPanel(
-      #        helpText("Look at locations of the trending hashtag"),
-       #       textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
-        #      submitButton("Submit")
-         #   )))
+navbarPage("Aviato!", 
+  tabPanel("Map",
+           verticalLayout(
+             titlePanel("Aviato"),
+             mainPanel(
+               img(src="twitter.jpg", height = 100, width =150, align ="right")),
+             plotOutput('map'),
+             wellPanel(
+               helpText("Look at the locations of trending hashtags"),
+               textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
+               submitButton("Submit")
+             )
+             )
+           ),
+  tabPanel("Table",
+           DT::dataTableOutput("table"))
+  )
+
 #shinyUI()
 #8d06760fb918ea2ed65c16bab3b7b7295820a374
