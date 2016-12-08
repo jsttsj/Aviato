186b60f0a654dfce1784d58b9eb359af55130ef7
richkimzetapsi <richkim.kamlot@gmail.com>
2016-12-08 14:33:00 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,13 +5,16 @@ library(rsconnect)
 library(twitteR)
 library(rmarkdown)
 
-
+#navbar makes navigation bars for separate tabs 
 navbarPage("Aviato!", 
   tabPanel("Map",
+           #This makes the plotly output the main focus of the Map pannel
            verticalLayout(
              titlePanel("Locations of Trending Topics!!"),
              mainPanel(
+               #pulls out twitter logo 
                img(src="twitter.jpg", height = 100, width =130, align ="right")),
+             #outputs the plotly map! 
              plotlyOutput('map'),
              wellPanel(
                helpText("Find the locations of trending hashtags or related keywords"),
@@ -20,8 +23,10 @@ navbarPage("Aviato!",
              )
              )
            ),
+           #new tab for the data table 
   tabPanel("Table",
            dataTableOutput("table")), 
+           #new tab that explains the whole project 
   tabPanel("About",
            includeMarkdown("About.md"))
   )
