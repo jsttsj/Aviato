cdb68ab40836e5b3ec2a96f3442c7a37473f8896
Jenny Liang <Jenny@D-173-250-135-157.dhcp4.washington.edu>
2016-12-07 18:57:22 -0800

    Bug fixes.

--- ui.R
+++ ui.R
@@ -10,16 +10,16 @@ navbarPage("Aviato!",
              titlePanel("Aviato"),
              mainPanel(
                img(src="twitter.jpg", height = 100, width =150, align ="right")),
-             plotOutput('map'),
+             plotlyOutput('map'),
              wellPanel(
                helpText("Look at the locations of trending hashtags"),
                textInput("search", label = h3("Enter Hashtag"), value = "#Hashtag"),
                submitButton("Submit")
              )
              )
-           ),
-  tabPanel("Table",
-           DT::dataTableOutput("table"))
-  )
+           ))
+#  tabPanel("Table",
+ #          DT::dataTableOutput("table"))
+  #)
 #shinyUI()
 #8d06760fb918ea2ed65c16bab3b7b7295820a374
